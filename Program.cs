619bc5e161c57c46b25e2ba3498dd8a53462391a
Abdulrahman <abdulrahman.m.hallak.293@gmail.com>
2023-04-10 22:26:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿namespace TeleprompterConsole;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        RunTeleprompter().Wait();
+    }
+
+
+    public static IEnumerable<string> ReadFrom(string filePath)
+    {
+        string? line;
+        using StreamReader reader = new StreamReader(filePath);
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            var words = line.Split(' ');
+            var lineLength = 0;
+            foreach (var word in words)
+            {
+                yield return word + " ";
+                lineLength += word.Length + 1;
+
+                if (lineLength > 70)
+                {
+                    yield return Environment.NewLine;
+                    lineLength = 0;
+                }
+            }
+            yield return Environment.NewLine;
+        }
+    }
+
+    public static async Task ShowTeleprompter(TeleprompterConfig config)
+    {
+        var words = ReadFrom("C:\\Users\\Abdulrahman\\Desktop\\New Text Document.txt");
+        foreach (var word in words)
+        {
+            Console.Write(word);
+            if (!string.IsNullOrEmpty(word))
+            {
+                await Task.Delay(config.DelayInMilliseconds);
+            }
+        }
+        config.SetDone();
+    }
+
+    private static async Task GetInput(TeleprompterConfig config)
+    {
+        Action work = () =>
+        {
+            do
+            {
+                var key = Console.ReadKey(true);
+                if (key.KeyChar == '<')
+                    config.UpdateDelay(10);
+
+                else if (key.KeyChar == '>')
+                    config.UpdateDelay(-100);
+
+                else if (key.KeyChar == 'X' || key.KeyChar == 'x')
+                    break;
+
+            } while (!config.Done);
+
+        };
+        await Task.Run(work);
+    }
+    private static async Task RunTeleprompter()
+    {
+        var config = new TeleprompterConfig();
+        var displayTask = ShowTeleprompter(config);
+
+        var speedTask = GetInput(config);
+        await Task.WhenAny(displayTask, speedTask);
+    }
+}
---
+++ TeleprompterConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace TeleprompterConsole;
+using static Math;
+internal class TeleprompterConfig
+{
+    public int DelayInMilliseconds { get; private set; } = 200;
+    public bool Done { get; private set; }
+
+    public void SetDone() => Done = true;
+
+    public void UpdateDelay(int increment) // Negative to speed up.
+    {
+        var newDelay = Min(DelayInMilliseconds + increment, 1000);
+        newDelay = Max(newDelay, 20);
+        DelayInMilliseconds = newDelay;
+    }
+
+}
+
